0c8c69b24c83a16aadba5f295a0b5660fa209dc7
Jessica B. Hamrick <jhamrick@berkeley.edu>
2015-11-20 12:04:56 -0800

    Make instructor subfolder

---
+++ instructor/nbgrader_config.py
@@ -0,0 +1,4 @@
+c = get_config()
+
+c.NbGrader.course_id = "demo"
+c.IncludeHeaderFooter.header = 'source/header.ipynb'
--- nbgrader_config.py
+++ nbgrader_config.py
@@ -1,4 +1,3 @@
 c = get_config()
 
 c.NbGrader.course_id = "demo"
-c.IncludeHeaderFooter.header = 'source/header.ipynb'
